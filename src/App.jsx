2b3d6852c22f9eec9730271ea44dc8abde04b3c0
khushi <kanwaruma298@gmail.com>
2024-11-22 14:19:04 +0530

    project_initiated_on22nd2024

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//margin
+
+// const App = () => {
+//   return (
+//    <>
+//     <div className="my-8 bg-red-500">
+//       <div>HELLO</div>
+//     </div>
+//     <div className="-mt-[88px] bg-green-200">
+//       <div>HELLO</div>
+//     </div>
+//    </>
+//   )
+// }
+
+const App = () => {
+  return (
+   <>
+    <div className="p-[362px] bg-red-600 text-center text-white ">
+      <h1 className='text-center text-5xl tracking-widest'>NETFLIX</h1>
+    </div>
+   </>
+  )
+}
+
+export default App                                              
