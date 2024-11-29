ee44389af5215825f5fc0852e087ecf0e8467e79
khushi <kanwaruma298@gmail.com>
2024-11-29 13:08:21 +0530

    29 nov

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,68 @@ import React from 'react'
 //   )
 // }
 
-const App = () => {
+
+// position fixed 
+
+// const App = () => {
+//   return (
+//    <>
+//     <div className='bg-red-400 p-8 fixed w-full'>1</div>
+//     <div className='bg-yellow-400 h-[120rem]'>2</div>
+//    </>
+//   )
+// }
+
+// export default App 
+
+
+//position absolute
+
+// const App = () => {
+//   return (
+//    <>
+//     <div className='bg-red-600 h-screen w-full relative'>1</div>
+//     <div className='bg-yellow-600 h-24 w-24 absolute bottom-0 right-0'></div>
+//     <div className='bg-yellow-600 h-24 w-24 absolute bottom-0 '></div>
+//     <div className='bg-yellow-600 h-24 w-24 absolute top-0 '></div>
+//     <div className='bg-yellow-600 h-24 w-24 absolute top-0 right-0'></div>
+//    </>
+//   )
+// }
+
+//export default App 
+
+//icons
+
+
+//  const App = () => {
+//   return (
+//    <>
+//     <div className='bg-indigo-900 h-screen w-full '></div>
+//     <div className='bg-yellow-600 size-24 rounded-full absolute top-60'></div>
+//     <div className='bg-pink-600 size-24 rounded-full absolute top-60'></div>
+//     <div className='bg-green-600 size-24 rounded-full absolute top-60'></div>
+//    </>
+//   )
+// }
+
+// export default App 
+                                             
+
+ const App = () => {
   return (
    <>
-    <div className="p-[362px] bg-red-600 text-center text-white ">
-      <h1 className='text-center text-5xl tracking-widest'>web</h1>
+    <div className='bg-blue-800 w-full h-24 p-8'>
+    <div>
+      <ul className='gap-7 flex justify-center [&>*]:text-white font-bold text-xl'>
+        <li className='cursor-pointer '>Home</li>
+        <li className='cursor-pointer '>About</li>
+        <li className='cursor-pointer '>Contact</li>
+      </ul>
+    </div>
     </div>
    </>
   )
 }
 
 export default App 
-                                             
